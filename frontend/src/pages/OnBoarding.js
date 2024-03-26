5c9327a09bf03d6cccea9f6065568424afd14fc2
codeabuu <abdulngui718@gmail.com>
2024-03-26 13:24:37 -0400

    Create an onboarding form with user data submission and photo upload feature

---
+++ frontend/src/pages/OnBoarding.js
@@ -0,0 +1,217 @@
+import Nav from '../components/Nav'
+import {useState} from 'react'
+import {useCookies} from 'react-cookie'
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
+
+const OnBoarding = () => {
+    const [cookies, setCookie, removeCookie] = useCookies(['UserId'])
+    const [formData, setFormData] = useState({
+        user_id: cookies.UserId,
+        first_name: "",
+        dob_day: "",
+        dob_month: "",
+        dob_year: "",
+        show_gender: false,
+        gender_identity: "man",
+        gender_interest: "woman",
+        url: "",
+        about: "",
+        matches: []
+
+    })
+
+    let navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        console.log('submitted')
+        e.preventDefault()
+        try {
+            const response = await axios.put('http://localhost:8000/user', {formData})
+            console.log(response)
+            const success = response.status === 200
+            if (success) navigate('/dashboard')
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+
+    const handleChange = (e) => {
+        console.log('e', e)
+        const value = e.target.type === "checkbox" ? e.target.checked : e.target.value
+        const name = e.target.name
+
+        setFormData((prevState) => ({
+		...prevState,
+            [name]: value
+        }))
+    }
+	 const handlePhotoUpload = (files) => {
+	        const file = files[0]; // Assuming only one file is selected
+       		 if (file) {
+            	const reader = new FileReader();
+            	reader.onloadend = () => {
+                	setFormData(prevState => ({
+                    		...prevState,
+                    		url: reader.result // Set the URL of the uploaded image
+               		 }));
+            	};
+            reader.readAsDataURL(file); // Read the file as data URL
+        }
+    };
+
+    return (
+        <>
+            <Nav
+                minimal={true}
+                setShowModal={() => {
+                }}
+                showModal={false}
+            />
+
+            <div className="onboarding">
+                <h2>CREATE ACCOUNT</h2>
+
+                <form onSubmit={handleSubmit}>
+                    <section>
+                        <label htmlFor="first_name">First Name</label>
+                        <input
+                            id="first_name"
+                            type='text'
+                            name="first_name"
+                            placeholder="First Name"
+                            required={true}
+                            value={formData.first_name}
+                            onChange={handleChange}
+                        />
+
+                        <label>Birthday</label>
+                        <div className="multiple-input-container">
+                            <input
+                                id="dob_day"
+                                type="number"
+                                name="dob_day"
+                                placeholder="DD"
+                                required={true}
+                                value={formData.dob_day}
+                                onChange={handleChange}
+                            />
+
+                            <input
+                                id="dob_month"
+                                type="number"
+                                name="dob_month"
+                                placeholder="MM"
+                                required={true}
+                                value={formData.dob_month}
+                                onChange={handleChange}
+                            />
+
+                            <input
+                                id="dob_year"
+                                type="number"
+                                name="dob_year"
+                                placeholder="YYYY"
+                                required={true}
+                                value={formData.dob_year}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <label>Gender</label>
+                        <div className="multiple-input-container">
+                            <input
+                                id="man-gender-identity"
+                                type="radio"
+                                name="gender_identity"
+                                value="man"
+                                onChange={handleChange}
+                                checked={formData.gender_identity === "man"}
+                            />
+                            <label htmlFor="man-gender-identity">Man</label>
+                            <input
+                                id="woman-gender-identity"
+                                type="radio"
+                                name="gender_identity"
+                                value="woman"
+                                onChange={handleChange}
+                                checked={formData.gender_identity === "woman"}
+                            />
+                            <label htmlFor="woman-gender-identity">Woman</label>
+                        </div>
+
+                        <label htmlFor="show-gender">Show Gender on my Profile</label>
+
+                        <input
+                            id="show-gender"
+                            type="checkbox"
+                            name="show_gender"
+                            onChange={handleChange}
+                            checked={formData.show_gender}
+                        />
+
+                        <label>Show Me</label>
+
+                        <div className="multiple-input-container">
+                            <input
+                                id="man-gender-interest"
+                                type="radio"
+                                name="gender_interest"
+                                value="man"
+                                onChange={handleChange}
+                                checked={formData.gender_interest === "man"}
+                            />
+                            <label htmlFor="man-gender-interest">Man</label>
+                            <input
+                                id="woman-gender-interest"
+                                type="radio"
+                                name="gender_interest"
+                                value="woman"
+                                onChange={handleChange}
+                                checked={formData.gender_interest === "woman"}
+                            />
+                            <label htmlFor="woman-gender-interest">Woman</label>
+
+                        </div>
+
+                        <label htmlFor="about">About me</label>
+                        <input
+                            id="about"
+                            type="text"
+                            name="about"
+                            required={true}
+                            placeholder="I like long walks..."
+                            value={formData.about}
+                            onChange={handleChange}
+                        />
+
+                        <input type="submit"/>
+                    </section>
+
+                    <section>
+
+                        <label htmlFor="url">Profile Photo</label>
+                        <input
+                            type="file"
+                            name="image/*"
+                            id="url"
+                            onChange={(e) => handlePhotoUpload(e.target.files)}
+                            required={true}
+			    style={{display: "none"}}
+                        />
+                        <div className="photo-container">
+                            {formData.url && <img src={formData.url} alt="profile pic preview"/>}
+                        </div>
+			 <button className="p-button" type="button" onClick={() => document.getElementById('url').click()}>
+                            Select Photo(must not exceed 10mb)
+                        </button>
+
+                    </section>
+
+                </form>
+            </div>
+        </>
+    )
+}
+export default OnBoarding
