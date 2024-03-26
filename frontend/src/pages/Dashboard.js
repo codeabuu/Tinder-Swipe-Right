fc535d43d6cd8c92b545382aa379b4f145186b6a
codeabuu <abdulngui718@gmail.com>
2024-03-26 13:21:01 -0400

    Dashboard component with Tinder-like swiping and user matching

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,120 @@
+import TinderCard from 'react-tinder-card'
+import {useEffect, useState} from 'react'
+import ChatContainer from '../components/ChatContainer'
+import {useCookies} from 'react-cookie'
+import axios from 'axios'
+
+const Dashboard = () => {
+    const [user, setUser] = useState(null)
+    const [genderedUsers, setGenderedUsers] = useState(null)
+    const [lastDirection, setLastDirection] = useState()
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
+
+    const userId = cookies.UserId
+
+
+    const getUser = async () => {
+        try {
+	    if (!userId) return
+            const response = await axios.get('http://localhost:8000/user', {
+                params: {userId}
+            })
+            setUser(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const getGenderedUsers = async () => {
+        try {
+	    if (!userId || !user) return
+            const response = await axios.get('http://localhost:8000/gendered-users', {
+                params: {gender: user?.gender_interest}
+            })
+	    
+            setGenderedUsers(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getUser()
+
+    }, [])
+
+    useEffect(() => {
+        if (user) {
+            getGenderedUsers()
+        }
+    }, [user])
+
+    const updateMatches = async (matchedUserId) => {
+        try {
+	    if (!userId) return
+            await axios.put('http://localhost:8000/addmatch', {
+                userId,
+                matchedUserId
+            })
+            getUser()
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+
+    const swiped = (direction, swipedUserId) => {
+        if (direction === 'right') {
+            updateMatches(swipedUserId)
+        } else if (direction === 'left') {
+	     setGenderedUsers(prevGenderedUsers =>
+		     prevGenderedUsers.filter(user => user.user_id !== swipedUserId)
+	     )
+	}
+        setLastDirection(direction)
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+    }
+
+    const matchedUserIds = user?.matches.map(({user_id}) => user_id).concat(userId)
+
+    const filteredGenderedUsers = genderedUsers?.filter(genderedUser => !matchedUserIds.includes(genderedUser.user_id))
+
+
+    console.log('filteredGenderedUsers ', filteredGenderedUsers)
+    return (
+        <>
+            {user && filteredGenderedUsers !== null &&
+            <div className="dashboard">
+                <ChatContainer user={user}/>
+                <div className="swipe-container">
+                    <div className="card-container">
+
+                        {filteredGenderedUsers?.map((genderedUser) =>
+                            <TinderCard
+                                className="swipe"
+                                key={genderedUser.user_id}
+                                onSwipe={(dir) => swiped(dir, genderedUser.user_id)}
+                                onCardLeftScreen={() => outOfFrame(genderedUser.first_name)}>
+                                <div
+                                    style={{backgroundImage: "url(" + genderedUser.url + ")"}}
+                                    className="card">
+                                    <h3>{genderedUser.first_name}</h3>
+				<div className="buttons-cont">
+				   <button onClick={() => swiped('right', genderedUser.user_id)} className="tick-button">✓</button>
+				   <button onClick={() => swiped('left', genderedUser.user_id)} className="x-button">✗</button>
+				</div>
+                                </div>
+                            </TinderCard>
+                        )}
+                        <div className="swipe-info">
+                            {lastDirection ? <p></p> : <p/>}
+                        </div>
+                    </div>
+                </div>
+            </div>}
+        </>
+    )
+}
+export default Dashboard
