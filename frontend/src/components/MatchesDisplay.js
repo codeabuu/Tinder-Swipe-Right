c6120664e1e02ffd92c1ef186e8524baac82174b
lawrence1986 <madusquare@gmail.com>
2024-03-26 18:49:11 +0100

    Implemented MatchesDisplay component
    This commit introduces the MatchesDisplay component, responsible for rendering matched profiles based on user matches. It utilizes Axios for fetching data from the backend API endpoint '/users'. Upon receiving the data, it filters and displays matched profiles accordingly. The component tracks changes in the 'matches' prop using the useEffect hook to fetch updated matches when necessary. Additionally, it allows users to click on a matched profile to initiate a conversation.

---
+++ frontend/src/components/MatchesDisplay.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+
+const MatchesDisplay = ({ matches, setClickedUser }) => {
+  const [matchedProfiles, setMatchedProfiles] = useState([]);
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const userId = cookies.UserId;
+
+  const matchedUserIds = matches.map(({ user_id }) => user_id);
+
+  const getMatches = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/users", {
+        params: { userIds: JSON.stringify(matchedUserIds) },
+      });
+      setMatchedProfiles(response.data);
+    } catch (error) {
+      console.error("Error fetching matches:", error);
+      // You can add error handling here
+    }
+  };
+
+  useEffect(() => {
+    getMatches();
+  }, [matches]);
+
+  const filteredMatchedProfiles = matchedProfiles.filter(
+    (matchedProfile) =>
+      matchedProfile.matches.filter((profile) => profile.user_id === userId)
+  );
+
+  return (
+    <div className="matches-display">
+      {filteredMatchedProfiles.map((match, index) => (
+        <div
+          key={index}
+          className="match-card"
+          onClick={() => setClickedUser(match)}
+        >
+          <div className="img-container">
+            <img src={match?.url} alt={match?.first_name + " profile"} />
+          </div>
+          <h3>{match?.first_name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MatchesDisplay;
