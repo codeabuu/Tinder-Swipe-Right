e7fadf493c9f371e9f75d13286b09deefd34a9e6
Abdullahi Ngui <125456974+codeabuu@users.noreply.github.com>
2024-06-11 16:15:13 -0400

    Update MatchesDisplay.js

--- frontend/src/components/MatchesDisplay.js
+++ frontend/src/components/MatchesDisplay.js
@@ -11,7 +11,7 @@ const MatchesDisplay = ({ matches, setClickedUser }) => {
 
   const getMatches = async () => {
     try {
-      const response = await axios.get("http://localhost:8000/users", {
+      const response = await axios.get("https://tinder-swipe-right.onrender.com/users", {
         params: { userIds: JSON.stringify(matchedUserIds) },
       });
       setMatchedProfiles(response.data);
