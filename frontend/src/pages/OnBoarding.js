0647cecf9f81fd265b80e3de9b930d6f075f435a
Abdullahi Ngui <125456974+codeabuu@users.noreply.github.com>
2024-06-11 16:17:57 -0400

    Update OnBoarding.js

--- frontend/src/pages/OnBoarding.js
+++ frontend/src/pages/OnBoarding.js
@@ -27,7 +27,7 @@ const OnBoarding = () => {
         console.log('submitted')
         e.preventDefault()
         try {
-            const response = await axios.put('http://localhost:8000/user', {formData})
+            const response = await axios.put('https://tinder-swipe-right.onrender.com/user', {formData})
             console.log(response)
             const success = response.status === 200
             if (success) navigate('/dashboard')
